261d61be3dbd7fd3fef8054228fb0481f2258abb
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2020-02-20 21:20:29 -0500

    wip: restructure go project

--- server/main.go
+++ server/main.go
@@ -2,72 +2,24 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-
 	"github.com/gorilla/mux"
+	"github.com/routehandlers/routehandlerscd" //!this is wrong figuere out how to structure this. the project is broke
+	// u "net/url"
 )
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", baseRoute).Methods("GET")
+	r.HandleFunc("/", routehandlers.baseRoute).Methods("GET")
 
-	r.HandleFunc("/api/login", login).Methods("POST")
+	r.HandleFunc("/api/login", routehandlers.login).Methods("POST")
 
-	r.HandleFunc("/api/favorites/{userId}", favoritesByUserID).Methods("GET")
+	r.HandleFunc("/api/favorites/{userId}", rouotehandlers.favoritesByUserID).Methods("GET")
 
-	r.HandleFunc("/api/repos?q={term}", repos).Methods("GET")
+	r.HandleFunc("/api/repos?q={term}", routehandlers.repos).Methods("GET")
 
-	r.HandleFunc("/api/repos/{userId}", reposByUserID).Methods("GET")
+	r.HandleFunc("/api/repos/{userId}", rooutehandlers.reposByUserName).Methods("GET")
 
 	fmt.Println("Server listening!")
 	http.ListenAndServe(":9901", r)
-}
-
-func baseRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "damnit boy")
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "you trynna login")
-}
-
-func favoritesByUserID(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://api.github.com/users/JNaeemGitonga/starred")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Fprintf(w, string(body))
-}
-
-func reposByUserID(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://api.github.com/users/JNaeemGitonga/repos")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Fprintf(w, string(body))
-}
-
-func repos(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "trynna get all repos")
 }
